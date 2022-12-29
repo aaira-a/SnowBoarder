c702938fe4b4a7166b493cd18a1a790fbdf67ddc
aaira alias <aaaira.alias@gmail.com>
2022-12-30 00:11:56 +0800

    Set boost and base speed to Effector depending on Player input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,17 +5,34 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 20f;
     Rigidbody2D rb2d;
-    // Start is called before the first frame update
+    SurfaceEffector2D surfaceEffector2D;
+
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         RotatePlayer();
+        RespondToBoost();
+    }
+
+    void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            surfaceEffector2D.speed = baseSpeed;
+        }
     }
 
     void RotatePlayer()
@@ -29,6 +46,5 @@ public class PlayerController : MonoBehaviour
         {
             rb2d.AddTorque(-torqueAmount);
         }
-
     }
 }
